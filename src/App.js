206555ee4f2d20f389225662c2a824732073586e
Parth <parthkambli.pk1@gmail.com>
2022-07-24 11:36:43 +0530

    Intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
+import Intro from "./components/Intro/Intro.js";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>
+    <Intro />
+  </div>;
 }
 
 export default App;
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,26 @@
+import "./intro.css"
+
+function Intro() {
+
+  return (
+    <div className="Intro text-center">
+      <div className="container">
+        <div className="row">
+          <div className="IntorIMG col-lg-4">
+            <img src="https://i.ibb.co/T2d0qCw/parth.jpg" alt="Profile" />
+          </div>
+          <div className="IntroContent col-lg-8">
+            <h1>Hello!</h1>
+            <p>
+              I'm <span>Parth Kambli,</span> A ful stack webdeveloper passionate
+              about creating interactive applications and experiences on the
+              web.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
